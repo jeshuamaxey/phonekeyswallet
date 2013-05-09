5b355dd89b6b78afded43124063a246ba896100c
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-05-09 23:39:53 +0100

    edited css to allow for arbitray sizing of slides - event listeners added to buttons which detect a custom attribute of those buttons to take action with (this attribute may change - not sure that it's the best choice of variable paradigm)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
 	impress().init();
+
+	//add event listeners that will cue movements through the app
+	$('.btn-info.male').click(startMale);
+	$('.btn-info.female').click(startFemale);
+
+	$('.btn-success').click(yesClick);
+	$('.btn-danger').click(noClick);
 });
+
+function yesClick() {
+	console.log($(this).attr('whereNext'));
+}
+
+function noClick() {
+	console.log($(this).attr('whereNext'));
+}
+
+function startMale() {
+	console.log('male');
+}
+
+function startFemale() {
+	console.log('female');
+}
