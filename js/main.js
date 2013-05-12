c8ff490d2a7a2e89589c4cdca16a2a18d34094f3
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-05-12 18:50:23 +0100

    javascript now properly navigates user through flowchart based on their answers - direction keys need disabling still

--- js/main.js
+++ js/main.js
@@ -2,15 +2,33 @@ $(document).ready(function() {
 	impress().init();
 
 	//add event listeners that will cue movements through the app
+	/*
 	$('.btn-info.male').click(startMale);
 	$('.btn-info.female').click(startFemale);
 
 	$('.btn-success').click(yesClick);
 	$('.btn-danger').click(noClick);
+	*/
+	$('.btn').click(overlord);
 });
 
+function overlord() {
+	//stopPropagation required because clicking on an element in impress.js
+	//automatically makes that element's slide the focus. We want to stop
+	//this so we can control the user's navigation through the flowchart
+	event.stopPropagation();
+	var next = $(this).attr('whereNext');
+	console.log(next);
+	impress().goto(next);
+}
+
+/*
+
 function yesClick() {
-	console.log($(this).attr('whereNext'));
+	var next = $(this).attr('whereNext');
+	console.log(next);
+//	console.log(event);
+	impress().goto(next);
 }
 
 function noClick() {
@@ -24,3 +42,5 @@ function startMale() {
 function startFemale() {
 	console.log('female');
 }
+
+*/
